a0f5fabb3044e8ac283b990af0d43cb5b3cc0ac4
GayaneHovh85 <gayanehovhannisyan023@gmail.com>
2023-05-06 10:52:34 +0300

    new test file

---
+++ homework2/homework2.py
@@ -0,0 +1,43 @@
+a=int(input ("input first number"))
+
+if a % 2 == 0:
+    print ("a is zuyg")
+else:
+    print("a is kent")
+    if a% 3 ==0:
+        print("3in bajanarar")
+if a % 2 == 0 and a% 3 ==0:
+    print ("divides 2 and 3")
+
+    """ xndir 2"""
+x = int(input ("input first number"))
+y = int(input ("input second number"))
+if x > y:
+    print ("x is max")
+else: print ("y is max")
+
+
+
+""" xndir 3 um  3 lety petq e lini else ev ayln """
+
+a = int(input ("input first number"))
+b = int(input ("input second nu mber"))
+if a == b:
+    print ("a")
+    if a>b:
+        print("a is greater than b")
+else: print ("b is greater than a")
+
+
+x=4
+while x in range (40):
+    print (x%4==0)
+
+
+
+
+
+
+
+
+
